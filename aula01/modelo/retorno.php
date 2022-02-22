79b258653e31498b5b8cd57677a99bfba1096125
pauloAlvaresNeto <paulo.alvares01@etec.sp.gov.br>
2022-02-22 09:10:06 -0300

    commit - aula01

--- aula01/modelo/retorno.php
+++ aula01/modelo/retorno.php
@@ -1,1 +1,32 @@
 <?php
+
+$nome = $_REQUEST['nome'];
+$animal = $_REQUEST['animal'];
+
+if(empty($nome)){
+    "tipo" => 'error',
+    "mensagem" => "campos não definidos."
+);
+} else {
+    switch($animal) {
+        case '1' : $dados = array(
+            "tipo" => 'cachorro.jpg',
+            "mensagem" =>  "Teste" . $nome . '...'
+        );
+        break;
+
+        case '2' : $dados = array(
+            "tipo" => 'gato.jpg',
+            "mensagem" =>  "Teste" . $nome . '...'
+        );
+        break;
+
+        case '2' : $dados = array(
+            "tipo" => 'peixe.jpg',
+            "mensagem" =>  "Teste" . $nome . '...'
+        );
+        break;
+    }
+}
+
+echo json_encode($dados);
